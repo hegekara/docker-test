5f36cefb58130a375e68ad35b4b328a00e4cd229
Hilmi Ege Kara <hegekara48@gmail.com>
2026-04-05 13:17:32 +0300

    changed api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'http://192.168.62.2:3000';
 
 export const apiCall = async (endpoint, options = {}) => {
   const token = localStorage.getItem('token');
